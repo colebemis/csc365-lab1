5eff9ade22b4145f98267c1da7e8579687585de4
Kellie Banzon <kelliebanzon@gmail.com>
2019-04-08 10:09:25 -0700

    added parsing error messages

--- lab1.py
+++ lab1.py
@@ -37,8 +37,15 @@ def parse_file(filename):
 
 # parses the command and calls the appropriate function
 # note: quitting out of the program is handled in the main loop
-# TODO: add more comprehensive error menu messages
 def parse_cmd(cmd, data):
+  err_msg = "Possible commands:\n" \
+            "- S[tudent]: <lastname> [B[us]]\n" \
+            "- T[eacher]: <lastname>\n" \
+            "- B[us]: <number>\n" \
+            "- G[rade]: <number> [H[igh]|L[ow]]\n" \
+            "- A[verage]: <number>\n" \
+            "- I[nfo]\n" \
+            "- Q[uit]"
   cmd_words = cmd.split()
 
   query_length = len(cmd_words)
@@ -54,7 +61,7 @@ def parse_cmd(cmd, data):
       print("Querying student {} with bus info".format(cmd_words[1]))
       find_by_lastname_bus(cmd_words[1], data["lastname"])
     else:
-      print("Malformatted student query")
+      print("Unrecognized query.", err_msg)
 
   elif first_word == "teacher" or first_word == "t":
     print("Querying teacher {}".format(cmd_words[1]))
@@ -75,6 +82,8 @@ def parse_cmd(cmd, data):
         elif third_word == "low" or third_word == "l":
             print("Querying lowest GPA in grade {}".format(cmd_words[1]))
             find_by_grade_low(cmd_words[1], data["grade"])
+        else:
+          print("Unrecognized query.", err_msg)
 
   elif first_word == "average" or first_word == "a":
       print("Querying the average GPA for grade {}".format(cmd_words[1]))
@@ -85,7 +94,7 @@ def parse_cmd(cmd, data):
     summarize_by_grade(data["grade"])
 
   else:
-    print("Unrecognized query format")
+    print("Unrecognized query.", err_msg)
 
 
 def find_by_lastname(lastname, grouped_by_lastname):
